922f5f7580d91a824f89797c1e02d35626bfc0c2
CodeClod <mika9103@stud.kea.dk>
2022-02-25 09:35:50 +0100

    Added askAboutAge + printout

--- src/Exercise.java
+++ src/Exercise.java
@@ -9,14 +9,21 @@ public class Exercise {
         return nameInput;
     }
 
+    int askAboutAge() {
+        System.out.println("What's your age?: ");
+        int age = in.nextInt();
+        return age;
+    }
+
     public static void main(String[] args) {
         Exercise prog = new Exercise();
-
 
         String name = prog.greet();
 
         System.out.println("It Works!");
-
         System.out.println("Hej " + name);
+
+        int age = prog.askAboutAge();
+        System.out.println("Your age is " + age);
     }
 }
