4e49c3a9e2ce6698e618c7377f7df1ecf9605820
CodeClod <mika9103@stud.kea.dk>
2022-02-25 09:31:19 +0100

    Tweaked greet and added Hej + name

--- src/Exercise.java
+++ src/Exercise.java
@@ -4,6 +4,7 @@ public class Exercise {
     Scanner in = new Scanner(System.in);
 
     String greet() {
+        System.out.println("What's you name?");
         String nameInput = in.nextLine();
         return nameInput;
     }
@@ -11,8 +12,11 @@ public class Exercise {
     public static void main(String[] args) {
         Exercise prog = new Exercise();
 
+
         String name = prog.greet();
 
         System.out.println("It Works!");
+
+        System.out.println("Hej " + name);
     }
 }
