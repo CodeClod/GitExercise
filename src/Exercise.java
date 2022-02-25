165b26bb9a122437822933418b8dab6ecfad11f2
CodeClod <mika9103@stud.kea.dk>
2022-02-25 09:29:26 +0100

    added greet

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Exercise {
+    Scanner in = new Scanner(System.in);
+
+    String greet() {
+        String nameInput = in.nextLine();
+        return nameInput;
+    }
+
     public static void main(String[] args) {
+        Exercise prog = new Exercise();
+
+        String name = prog.greet();
+
         System.out.println("It Works!");
     }
 }
